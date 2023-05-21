33b2ff3b89747746b8b1a3fd31f74fe7848c6f59
Mirt12 <Mirt12@yandex.ru>
2023-05-21 16:15:01 +0200

    Add greetings for cycle in Main class

--- src/main/java/by/itacademy/tatjanabalashevich/Main.java
+++ src/main/java/by/itacademy/tatjanabalashevich/Main.java
@@ -2,6 +2,7 @@ package by.itacademy.tatjanabalashevich;
 
 public class Main {
     public static void main(String[] args) {
+
         User user1 = new User();
         user1.name = "Misha";
         user1.password = "Hgf45_!";
@@ -10,19 +11,15 @@ public class Main {
         user2.name = "Nadja";
         user2.password = "Hgf47_!";
 
-        String greetings = "Hello World!";
+        String greetings = "Hello, ";
         System.out.println(greetings);
         System.out.println(user1);
         System.out.println(user2);
 
-        int number = 5;
-        String str = "your result is:";
-        System.out.println(str + number);
-        System.out.println("----------");
 
         String[] names = {"Alexa", "Maria", "Olga"};
         for (int i = 0; i < names.length; i++) {
-            System.out.println("Respected " + names[i] + " - is Good!");
+            System.out.println(greetings + "respected " + names[i] +"!");
         }
 
         System.out.println("----------");
@@ -38,5 +35,6 @@ public class Main {
 
         System.out.println(account1);
         System.out.println(account2);
+
     }
 }
